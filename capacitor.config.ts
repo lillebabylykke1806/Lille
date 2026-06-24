c87f04a1d5234b8317fb59578c0addc70aa23199
Isabella <lillebabylykke@outlook.com>
2026-06-24 11:33:38 +0100

    Oppdaterer codemagic signing

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,11 @@
 import { CapacitorConfig } from '@capacitor/cli';
-
 const config: CapacitorConfig = {
-  appId: 'no.lilleapp',
+  appId: 'no.lilleapp.app',
   appName: 'Lille',
   webDir: 'out',
+  server: {
+    url: 'https://www.lilleapp.no',
+    cleartext: true,
+  },
 };
-
 export default config;
